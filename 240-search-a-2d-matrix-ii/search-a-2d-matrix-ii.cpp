d287e245f90fa016d19e0e7d8022366d1d01a8ef
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-04-22 14:57:15 +0530

    Time: 682 ms (5.08%) | Memory: 18.7 MB (67.36%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // linear search 
         int r = matrix.size();
         int c = matrix[0].size();
         for (int i=0; i<r; i++){
