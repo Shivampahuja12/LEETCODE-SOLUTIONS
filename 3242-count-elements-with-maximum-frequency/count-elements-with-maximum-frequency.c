4958e75d6c4c389de830da29b5b4b2ffde36c705
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-05-24 14:11:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (9.09%) - LeetSync

---
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.c
@@ -0,0 +1,19 @@
+int maxFrequencyElements(int* nums, int numsSize) {
+    int freq[101] = {0};
+    for (int i=0; i<numsSize; i++){
+        freq[nums[i]]++;
+    }
+    int sum = 0;
+    int max = 0;
+    for (int i=0; i<101; i++){
+        if (freq[i] > max) {
+            max = freq[i];
+            sum = max;
+            // printf("%d", sum);
+        }
+        else if (freq[i] == max){
+            sum += freq[i];
+        }
+    }
+    return sum;
+}
