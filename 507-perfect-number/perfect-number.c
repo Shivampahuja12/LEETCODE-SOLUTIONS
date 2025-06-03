60fe91810b78c9f99fe4fea7276447121221d758
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-06-03 20:07:04 +0530

    Time: 1046 ms (31.57%) | Memory: 7.7 MB (84.30%) - LeetSync

---
+++ 507-perfect-number/perfect-number.c
@@ -0,0 +1,8 @@
+bool checkPerfectNumber(int num) {
+    int sum = 0;
+    for (int i=1; i<(num/2)+1; i++){
+        if (num%i==0) sum+=i;
+    }
+    if (num == sum) return true;
+    else return false;
+}
