53681822b67dde1f52c7d9a5d865417478918ed9
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-08-02 18:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (48.62%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        /* int m = 0;
+        /*
+        // 1
+        int m = 0;
         vector<int> a = nums;
         for (int i=0; i<a.size(); i++){
             if (a[i] == 0) nums[m++] = 0;
@@ -13,21 +15,45 @@ public:
             if (a[i] == 2) nums[m++] = 2;
         } */
 
+        /* // 2 - counting method
+        int cntZero = 0;
+        int cntOne = 0;
+        int cntTwo = 0;
 
-    // 2 - counting method
-    int cntZero = 0;
-    int cntOne = 0;
-    int cntTwo = 0;
-    
-    for (int i=0; i<nums.size(); i++){
-        if (nums[i] == 0) cntZero++;
-        else if (nums[i] == 1) cntOne++;
-        else if (nums[i] == 2) cntTwo++;
-    }
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0)
+                cntZero++;
+            else if (nums[i] == 1)
+                cntOne++;
+            else if (nums[i] == 2)
+                cntTwo++;
+        }
 
-    int m = 0;
-    for (int i=0; i<cntZero; i++) {nums[m++] = 0;}
-    for (int i=0; i<cntOne; i++) {nums[m++] = 1;}
-    for (int i=0; i<cntTwo; i++) {nums[m++] = 2;}
+        int m = 0;
+        for (int i = 0; i < cntZero; i++) {
+            nums[m++] = 0;
+        }
+        for (int i = 0; i < cntOne; i++) {
+            nums[m++] = 1;
+        }
+        for (int i = 0; i < cntTwo; i++) {
+            nums[m++] = 2;
+        } */
+
+        // 3 - three pointer approach
+        int lo = 0;
+        int mid = 0;
+        int hi = nums.size() - 1;
+        while (mid <= hi) {
+            if (nums[mid] == 0) {
+                swap(nums[lo], nums[mid]);
+                lo++, mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[hi]);
+                hi--;
+            }
+        }
     }
 };
