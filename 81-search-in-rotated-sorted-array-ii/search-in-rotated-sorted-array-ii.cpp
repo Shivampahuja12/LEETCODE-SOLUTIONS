4725b1ae87b9f782361b8625d30f60e06ba00fa8
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-09-11 20:03:51 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (89.50%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.cpp
@@ -1,18 +1,54 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
+
+        // brute - as we are only checking for the element is appeared in the array or not so, first sort the array and find it later using binary search 
+
+        // TC - O(nlogn);
+        // SC - O(1);
+        /* sort(nums.begin(), nums.end());
         int lo = 0;
-        int hi = nums.size()-1;
+        int hi = nums.size() - 1;
+        int mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (nums[mid] == target)
+                return true;
+            else if (nums[mid] > target) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return false; */
+
+
+        // optimized 
+        int lo = 0, hi = nums.size()-1;
         int mid;
         while (lo<=hi){
             mid = lo + (hi-lo)/2;
-            if (nums[mid] == target) return true;
-            else if (nums[mid] > target){
-                hi = mid -1;
+            if (target == nums[mid]) return true;
+            else if (nums[lo] == nums[mid] && nums[mid] == nums[hi]){
+                lo++;
+                hi--;
+                continue;
+            }
+            else if (nums[lo] <= nums[mid]){
+                if (target >= nums[lo] && target < nums[mid]){
+                    hi = mid - 1;
+                }
+                else {
+                    lo = mid + 1;
+                }
             }
             else {
-                lo = mid + 1;
+                if (target > nums[mid] && target <= nums[hi]){
+                    lo = mid + 1;
+                }
+                else {
+                    hi = mid -1;
+                }
             }
         }
         return false;
