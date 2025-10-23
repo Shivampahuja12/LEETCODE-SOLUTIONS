8ee5a0ed3491f4642080b1048742a15087afdd5b
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-10-23 16:44:45 +0530

    Time: 2 ms (16.48%) | Memory: 44.5 MB (78.49%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -2,7 +2,7 @@ class Solution {
     public String restoreString(String s, int[] indices) {
         // Arrays.sort(indices);
 
-        StringBuilder sb = new StringBuilder(s);
+        // StringBuilder sb = new StringBuilder(s);
         StringBuilder result = new StringBuilder();
         
         for (int i=0; i<s.length(); i++){
