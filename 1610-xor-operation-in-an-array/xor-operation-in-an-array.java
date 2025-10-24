604d8d9221c3bc27cd8c3e356752c9a17a63950e
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-10-24 20:29:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (85.27%) - LeetSync

--- 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.java
@@ -1,6 +1,8 @@
 class Solution {
     public int xorOperation(int n, int start) {
 
+
+        /* // brute force
         int[] a = new int[n];
         for (int i=0; i<n; i++){
             a[i] = start + 2 * i;
@@ -11,6 +13,15 @@ class Solution {
             xori ^= a[i];
         }
 
-        return xori;
+        return xori; */
+
+
+        // better (not using extra space)
+        int res = start;
+        for (int i=1; i<n; i++){
+            res ^= start + 2 * i;
+        }
+
+        return res;
     }
 }
