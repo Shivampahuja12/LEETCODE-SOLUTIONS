be357fb5a993aa8c687e8a2e41c17879bea9262b
Shivam Pahuja <shivampahuja1212@gmail.com>
2025-12-04 00:07:37 +0530

    Time: 0 ms (100.00%) | Memory: 85 MB (35.84%) - LeetSync

---
+++ 932-monotonic-array/monotonic-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        if (nums.length == 1 || nums.length == 2)
+            return true;
+
+        if (nums[0] < nums[nums.length-1]) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] <= nums[i + 1]) {
+                    continue;
+                } else
+                    return false;
+            }
+        } else {
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] >= nums[i + 1]) {
+                    continue;
+                } else
+                    return false;
+            }
+        }
+
+        return true;
+    }
+}
