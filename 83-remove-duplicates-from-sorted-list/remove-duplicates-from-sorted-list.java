caba07b68fc11acc2940655d46a0234abce7966e
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-05-19 12:10:43 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (50.35%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -10,17 +10,17 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-        
+        if (head == null || head.next == null) return head;
         ListNode temp = head;
-
-        while (temp != null && temp.next != null){
-            if (temp.val == temp.next.val){
-                temp.next = temp.next.next;
-            }
-            else {
+        ListNode rpt = head.next;
+        while (temp != null && rpt != null) {
+            if (temp.val != rpt.val) {
+                temp.next = rpt;
                 temp = temp.next;
             }
+            rpt = rpt.next;
         }
+        temp.next = null;
         return head;
     }
 }
