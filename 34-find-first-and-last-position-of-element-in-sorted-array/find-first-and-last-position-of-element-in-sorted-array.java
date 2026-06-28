9b9a5ec80ebe62d8982a6e2d31a4343a8ce5f228
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-06-28 14:31:35 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (13.49%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,46 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
+        int lo = 0;
+        int hi = nums.length - 1;
         int[] ans = new int[2];
-        ans[0] = first(nums, nums.length, target);
-        ans[1] = last(nums, nums.length, target);
+        ans[0] = firstOccurance(lo, hi, target, nums);
+        ans[1] = lastOccurance(lo, hi, target, nums);
         return ans;
     }
-    public static int first(int[] a, int n, int k){
-        int lo = 0;
-        int hi = n-1;
-        int mid;
+
+    public int firstOccurance(int lo, int hi, int target, int[] arr) {
         int ans = -1;
-        while (lo <= hi){
-            mid = lo + (hi-lo)/2;
-            if (a[mid] == k){
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == target) {
                 ans = mid;
-                hi = mid-1;
-            }
-            if (a[mid] < k) {
-                lo = mid+1;
-            }
-            if (a[mid] > k){
-                hi = mid-1;
+                hi = mid - 1;
+            } else if (arr[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
             }
         }
         return ans;
     }
-    public static int last(int[] a, int n, int k){
-        int lo = 0;
-        int hi = n-1;
-        int mid;
+
+    public int lastOccurance(int lo, int hi, int target, int[] arr) {
         int ans = -1;
-        while (lo <= hi){
-            mid = lo + (hi-lo)/2;
-            if (a[mid] == k){
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (arr[mid] == target) {
                 ans = mid;
-                lo = mid+1;
-            }
-            if (a[mid] < k) {
-                lo = mid+1;
-            }
-            if (a[mid] > k){
-                hi = mid-1;
+                lo = mid + 1;
+            } else if (arr[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
             }
         }
         return ans;
