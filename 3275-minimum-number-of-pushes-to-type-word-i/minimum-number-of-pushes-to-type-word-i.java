2e759a15d09a8d71a5af2dea4fd763669acefc9e
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-07-30 10:00:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (66.51%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.java
@@ -2,10 +2,7 @@ class Solution {
     public int minimumPushes(String word) {
         int totalCount = 0;
         for (int i = 0; i < word.length(); i++) {
-            if (i < 8) totalCount += 1;
-            else if (i < 16) totalCount += 2;
-            else if (i < 24) totalCount += 3;
-            else totalCount += 4;
+            totalCount += (i/8) + 1;
         }
         return totalCount;
     }
