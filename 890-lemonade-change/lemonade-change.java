4aba117827c96e6a582248a76a958e45867e070b
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-08-16 14:43:01 +0530

    Time: 2 ms (98.04%) | Memory: 72.5 MB (49.34%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -1,30 +1,30 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(5, 0);
-        map.put(10, 0);
-        map.put(20, 0);
-        for (int i = 0; i < bills.length; i++) {
-            if (bills[i] == 5) {
-                map.put(5, map.get(5) + 1);
-            } else if (bills[i] == 10) {
-                if (map.get(5) == 0) {
-                    return false;
-                }
-                map.put(5, map.get(5) - 1);
-                map.put(10, map.get(10) + 1);
-            } else {
-                if (map.get(10) > 0 && map.get(5) > 0) {
-                    map.put(10, map.get(10) - 1);
-                    map.put(5, map.get(5) - 1);
-                } else if (map.get(5) >= 3) {
-                    map.put(5, map.get(5) - 3);
-                } else {
-                    return false;
-                }
-                map.put(20, map.get(20) + 1);
+        int five = 0;
+        int ten = 0;
+        for (int i=0; i<bills.length; i++){
+            if (bills[i] == 5){
+                five++;
             }
-
+            else if (bills[i] == 10){
+                if (five == 0) return false;
+                else {
+                    ten++;
+                    five--;
+                }
+            }
+            else {
+                if (ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                }
+                else if (five >= 3){
+                    five-=3;
+                }
+                else {
+                    return false;
+                }
+            }
         }
         return true;
     }
