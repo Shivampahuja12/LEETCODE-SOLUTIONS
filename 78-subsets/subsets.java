97a171db44b6fe144787b8498aec12f45c0ee0f6
Shivam Pahuja <shivampahuja1212@gmail.com>
2026-08-29 17:41:31 +0530

    Time: 1 ms (86.49%) | Memory: 44 MB (88.25%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        List<Integer> smlList = new ArrayList<>();
-        subs(0, nums, list, smlList);
+        List<Integer> innerList = new ArrayList<>();
+        set(0, list, nums, innerList);
         return list;
     }
-    public static void subs(int i, int[] a, List<List<Integer>> list, List<Integer> smlList){
-        if (i == a.length) {
-            list.add(new ArrayList(smlList));
+
+    void set(int i, List<List<Integer>> list, int[] arr, List<Integer> innerList) {
+        if (i >= arr.length) {
+            list.add(new ArrayList(innerList));
             return;
         }
 
         // include
-        smlList.add(a[i]);
-        subs(i+1, a, list, smlList);
+        innerList.add(arr[i]);
+        set(i + 1, list, arr, innerList);
 
-        // exclude
-        smlList.remove(smlList.size() - 1);
-        subs(i+1, a, list, smlList);
+        //exclude
+        innerList.remove(innerList.size() - 1);
+        set(i + 1, list, arr, innerList);
+
     }
 }
